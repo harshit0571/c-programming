169e63d50227a34e405bcb11575c2925e5bfc957
harshit0571 <harshitchadha65@gmail.com>
2023-01-04 13:38:25 +0530

    first 4jan

---
+++ first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=10, b=20;
+    int sum=0;
+    sum=a+b;
+    // /t = 8 spaces
+    printf("chitkara\tuniversity \nharshit \n");
+    printf("%d", sum);
+}
