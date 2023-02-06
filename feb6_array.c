7ce40644fcf28f6003597162733bd75521e5c360
harshit0571 <harshitchadha65@gmail.com>
2023-02-06 14:36:06 +0530

    arrays

--- feb6_array.c
+++ feb6_array.c
@@ -4,12 +4,14 @@ int main()
     int n;
     scanf("%d", &n);
     int arr[n];
+    int sum = 0;
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", arr[i]);
+        sum += arr[i];
     }
+    printf(" %d ", sum);
 }
