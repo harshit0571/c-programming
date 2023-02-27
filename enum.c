72b32bb1295b91947c73c8c54cce49c500262f5e
harshit0571 <harshitchadha65@gmail.com>
2023-02-27 13:56:26 +0530

    file handling

--- enum.c
+++ enum.c
@@ -2,7 +2,7 @@
 
 enum year
 {
-    Jan=,
+    Jan,
     Feb,
     Mar,
     Apr,
---
+++ filehandling.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fp;
+    char file[100];
+    printf("enter file name \n");
+    gets(file);
+    printf("enter data \n");
+    char data[100];
+    gets(data);
+    fp = fopen(file, "w");
+    fputs(data, fp);
+}
